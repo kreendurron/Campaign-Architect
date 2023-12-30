81c48392126a9b400dabbc2cdb7f420caae0d3a7
Revival-Library <kreendurron@gmail.com>
2023-12-30 02:48:35 +0000

    Pinged mongodb successfully.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
 
+app = FastAPI()
+
+# Load environment variables from .env file
+load_dotenv()
+
+from pymongo.mongo_client import MongoClient
+uri = os.getenv("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
